00e6cda3c20d84640640cea37495743d53012d33
Yoann <ybuzenet@gmail.com>
2022-02-14 22:51:56 +0100

    test deviceMotion

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import SensorDisplay from "./components/sensor-display";
 import GyroscopeCompo from "./components/gyroscope";
+import Compass from "./components/magnetometer";
+import DeviceMotionComp from "./components/deviceMotion";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <SensorDisplay /> */}
-      <GyroscopeCompo />
+      {/* <GyroscopeCompo /> */}
+      {/* <Compass /> */}
+      <DeviceMotionComp />
     </View>
   );
 }
