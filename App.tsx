417754f81009ab0ab4b02729e1d7db61eeb1f028
Yoann <ybuzenet@gmail.com>
2022-02-14 22:27:12 +0100

    snsors from expo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SensorDisplay from "./components/sensor-display";
+import GyroscopeCompo from "./components/gyroscope";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <SensorDisplay /> */}
+      <GyroscopeCompo />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
